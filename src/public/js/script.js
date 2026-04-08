f219b2c1b8b082e55c0dfe91c821127de71fa1d0
FÈUE <feue.com1@outlook.com>
2026-04-08 08:48:12 +0200

    Update script.js

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -14,3 +14,14 @@
       document.body.style.fontFamily = font;
       pageTitle.style.fontFamily = font;
     });
+
+  // Select all elements with class 'year'
+  const yearElements = document.querySelectorAll('.year');
+
+  // Get current year
+  const currentYear = new Date().getFullYear();
+
+  // Set the text of each element to the current year
+  yearElements.forEach(el => {
+    el.textContent = currentYear;
+  });
