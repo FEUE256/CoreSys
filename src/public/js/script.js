a5372fea04d2985d383ddd4f26e79577b2772c99
FÈUE <feue.com1@outlook.com>
2026-04-08 08:49:47 +0200

    Update script.js

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,13 +1,6 @@
-    const toggleBtn = document.getElementById('toggleRoadmap');
+
     const fontSelector = document.getElementById('fontSelector');
     const pageTitle = document.getElementById('pageTitle');
-
-    toggleBtn.addEventListener('click', () => {
-      document.body.classList.toggle('light');
-      const isLight = document.body.classList.contains('light');
-      toggleBtn.textContent = isLight ? 'Switch to Dark Mode' : 'Switch to Light Mode';
-      toggleBtn.setAttribute('aria-pressed', isLight.toString());
-    });
 
     fontSelector.addEventListener('change', () => {
       const font = fontSelector.value;
